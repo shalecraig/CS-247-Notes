77ee8544d55d3634fbc9b381be71a7d51145b61f
Shale Craig <shalecraig@gmail.com>
2012-06-04 16:44:40 -0400

    small change

--- notes/14_make.sh
+++ notes/14_make.sh
@@ -6,7 +6,7 @@
 #       - does guard statements and includes (+macros).
 #   - compile individule modules (and all header and this their dependencies)
 
-# Recompile main.cpp, output to main.o.
+# Recompile main.cpp output to main.o.
 g++ -c main.cpp -o main.o
 
 # "Link" main.o with DT1.o, ADT2.o, and put into a file named exec.
