d67ea2f4caa1c675e24609473339e972a6581e54
shalecraig <shalecraig@gmail.com>
2012-06-12 15:37:49 -0400

    more notes

--- notes/18_namespaces_example.cpp
+++ notes/18_namespaces_example.cpp
@@ -30,3 +30,42 @@ void f2() {
     // X::k ++
     k+=1;
 }
+
+/* snip */
+// ------------------------------------------------------------------------------------------------
+// We want to protect local names against possible name clashes, so an nunamed name=space is used to declare local (private) methods in functions.
+// 
+// This generates a namespace like "namespace $$-asdfkkl1==" in the namespace - and it won't be able to be called from the outside.
+// 
+// This also generates a "use namespace $$-asdfkkl1==" directly after the namespace.
+namespace {
+    void foo();
+}
+
+// **Namespace lookup (resolution) rules:**
+// 
+// * Local Scope
+// 
+// * Enclosing scopes (inner -> outer), global namespace is last.
+// 
+// * Namespaces of all of function's arguments are last.
+// 
+/* snip */
+
+namespace Chrono {
+    class Date {
+        ...
+    };
+
+    ...
+    bool operator ==(const Date&, const std:: string);
+    ...
+}
+
+void f(Chrono::Date d, std::strings) {
+    // This equality operator is resolved towards:
+    // Local, then enclosing scopes, then namespaces of the arguments of the function 9i.e. std::string's namespace -> string
+    if (d == ...) {
+
+    }
+}
---
+++ notes/19_uml.cpp
@@ -0,0 +1,36 @@
+// **UML**: Unified Markup Language
+// 
+// Relevant: [Martin Fowler's UML textbook](http://todo.com)
+// 
+----------------------------------------------
+| name                                       |
+----------------------------------------------
+| fields                                     |
+----------------------------------------------
+| + functions                                |
+----------------------------------------------
+// In functions, there are different shapes that indicate public/private/etc:
+// 
+//  * \+ public
+// 
+//  * \- private
+// 
+//  * \# protected
+// 
+//  * static (underlined)
+// 
+//  * _pure virtual_
+// 
+// 
+// ------------------------------------------------
+// 
+// ***Association***
+// 
+// * A line means that there is an association between classes
+// 
+// * With addition of arrow heads, we have movement (info) i.e. who knows about who.
+// 
+// 
+// ------------------------------------------------
+// 
+// ***Multiplicity***
